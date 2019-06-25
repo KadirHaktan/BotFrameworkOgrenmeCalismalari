39a5342818e86930ce9ff4d8237705e1aa293014
Hacktin <isues41@gmail.com>
2019-06-25 14:06:17 +0300

    BotAccessor sinifiolusturuldu

---
+++ DialogBot/BotAccessors.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Bot.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DialogBot
+{
+    public class BotAccessors
+    {
+
+        public ConversationState _state { get; set; }
+
+        public BotAccessors(ConversationState _conversationState)
+        {
+            this._state = _conversationState ?? throw new ArgumentNullException(nameof(_state));
+        }
+
+        //public IStatePropertyAccessor<>
+        //public IStatePropertyAccessor<>
+
+        //public static string AccessorName1
+        //public static string AccessorName2
+
+
+    }
+}
