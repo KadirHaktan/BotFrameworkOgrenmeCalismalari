4277f001eedc235bcd06069aa9b9eabdd80c8bc4
Hacktin <isues41@gmail.com>
2019-06-26 13:22:19 +0300

    Diyaloglar için gerekli altyapı olusturuldu

---
+++ DialogBot/Bots/BankStateBot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DialogBot.Bots
+{
+    public class BankStateBot
+    {
+        public int Amount { get; set; }
+        public string Receipient { get; set; }
+    }
+}
