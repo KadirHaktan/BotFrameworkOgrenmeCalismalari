93827b74319965c61c70fd689cd3c8e83b898f6d
Hacktin <isues41@gmail.com>
2019-06-26 13:26:20 +0300

    MainDialog'un yapici metodu ile baslandi

--- DialogBot/Dialogs/MainDialog.cs
+++ DialogBot/Dialogs/MainDialog.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
 
 namespace DialogBot.Dialogs
 {
-    public class MainDialog
+    public class MainDialog:WaterfallDialog
     {
+        public MainDialog(string dialogID,IEnumerable<WaterfallStep> step = null):base(dialogID,step)
+        {
+
+        }
     }
 }
