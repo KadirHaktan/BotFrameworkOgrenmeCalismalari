e2509f8afa2c44ce954ba84b946c49805239c592
Hacktin <isues41@gmail.com>
2019-06-26 18:00:12 +0300

    MakePaymentDialog düzenlendi

--- DialogBot/Dialogs/Payment/MakePaymentDialog.cs
+++ DialogBot/Dialogs/Payment/MakePaymentDialog.cs
@@ -11,8 +11,45 @@ namespace DialogBot.Dialogs.Payment
         public MakePaymentDialog(string dialogid,IEnumerable<WaterfallStep> steps = null) : base(dialogid, steps)
         {
 
+            AddStep(async (stepContext, cancellianToken) =>
+            {
+                return await stepContext.PromptAsync(dialogid, new PromptOptions
+                {
+                    Prompt=stepContext.Context.Activity.CreateReply("Who would like to pay?")
+                });
+
+            });
+
+
+            AddStep(async (stepContext, cancellianToken) =>
+            {
+
+                var state = await (stepContext.Context.TurnState["BotAccessors"] as BotAccessors).BankStateBotAccessor.GetAsync(stepContext.Context);
+
+                state.Receipient = stepContext.Result.ToString();
+
+
+                return await stepContext.PromptAsync(dialogid, new PromptOptions
+                {
+                    Prompt = stepContext.Context.Activity.CreateReply($"{state.Receipient} got it{Environment.NewLine} How much should I pay"),
+                    RetryPrompt = stepContext.Context.Activity.CreateReply("Sorry,please give a number")
+                });
+            });
+
+
+            AddStep(async (stepContext, cancellianToken) =>
+            {
+                var state = await (stepContext.Context.TurnState["BotAccessors"] as BotAccessors).BankStateBotAccessor.GetAsync(stepContext.Context);
+                state.Amount = Int16.Parse(stepContext.Result.ToString());
+
+
+                await stepContext.Context.SendActivityAsync($" Thank you,I've paid {state.Amount} to {state.Receipient}");
+                return await stepContext.EndDialogAsync();
+
+            });
         }
 
         public static string Id => "MakePaymentDialog";
+        public static MakePaymentDialog Instance = new MakePaymentDialog(Id);
     }
 }
